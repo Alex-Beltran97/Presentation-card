448728ac4bd161c90aef4db77da5e06046d445e1
Alex-Beltran97 <92172762+Alex-Beltran97@users.noreply.github.com>
2021-12-09 08:00:37 -0500

    Presentation card

---
+++ app.js
@@ -0,0 +1,11 @@
+let app = new Vue ({
+	el:"#app",
+
+	data(){
+		return{
+			name:"Alexander Beltrán Hernández",
+			skill:"Front end Web developer",
+			skills: ["HTML","CSS","JavaScript","Vue.js"]
+		}
+	}
+})
